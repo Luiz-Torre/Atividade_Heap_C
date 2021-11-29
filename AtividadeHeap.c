07129e2d751015633ab5dd67eebb736670ca406a
Luiz Torre <luiztorre@id.uff.br>
2021-11-29 16:34:51 -0300

    criando o Remove Elemento

--- AtividadeHeap.c
+++ AtividadeHeap.c
@@ -7,10 +7,13 @@ int TOTAL_ELEMENTOS;
 
 void InsereElemento(int valor, int * vetor);
 void ImprimeElemento(int * vetor);
+int RemoveElemento (int * vetor);
+
 int main(){
     TOTAL_ELEMENTOS = 0;
     int *vetor;
     vetor = (int * ) malloc(1*sizeof(int));
+
     int removido = 0, num, valor;
     printf("Digite 1, 2, 3 ou 4 :\n1- Inserir um elemento na heap\n2- Remover um elemento\n3- Imprimir a Heap\n4- Sair:\n");
     scanf("%d", &num);
@@ -20,12 +23,14 @@ int main(){
         switch (num)
         {
             case 1:
+
                 puts("Informe o elemento que deseja inserir");
                 scanf("%d", &valor);
                 InsereElemento(valor,vetor);
 
                 break;
             case 2:
+                removido = RemoveElemento(vetor);
                 printf("O numero removido foi: %d", removido);
                 break;
 
@@ -50,6 +55,7 @@ int main(){
 
 void InsereElemento(int valor, int * vetor){
     if(TOTAL_ELEMENTOS == 0){
+
         vetor[TOTAL_ELEMENTOS] = valor;
     }
     else{
@@ -65,4 +71,13 @@ void ImprimeElemento(int * vetor){
     for(int i=0;i<TOTAL_ELEMENTOS;i++){
         printf("%d ", vetor[i]);
     }
+}
+
+int RemoveElemento(int * vetor){
+    int aux = vetor[0];
+    vetor[0] = vetor[--TOTAL_ELEMENTOS];
+    vetor = (int*) realloc (vetor, TOTAL_ELEMENTOS * sizeof(int));
+    return aux;
+
+
 }
