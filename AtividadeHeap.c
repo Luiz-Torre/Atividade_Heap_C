6b2f3ef4ef789ab2c402d4a7d152a938a6329aae
Luiz Torre <luiztorre@id.uff.br>
2021-11-29 15:05:10 -0300

    Corrigindo erro na alocacao de memoria do tam_n

--- AtividadeHeap.c
+++ AtividadeHeap.c
@@ -10,6 +10,7 @@ int main(){
     int *vetor;
     vetor = NULL;
     int * tam_n;
+    tam_n = (int * ) malloc(1*sizeof(int));
     *tam_n = 0;
     int removido = 0, num, valor;
     printf("Digite 1, 2, 3 ou 4 :\n1- Inserir um elemento na heap\n2- Remover um elemento\n3- Imprimir a Heap\n4- Sair:\n");
@@ -24,14 +25,12 @@ int main(){
                 scanf("%d", &valor);
                 InsereElemento(valor,tam_n,vetor);
 
-
                 break;
             case 2:
                 printf("O numero removido foi: %d", removido);
                 break;
 
             case 3:
-                puts("\nHeap impressa aqui");
                 ImprimeElemento(tam_n,vetor);
                 break;
 
@@ -43,6 +42,7 @@ int main(){
                 puts("\nNúmero invalido.");
                 break;
         }
+        printf("\nAAA : %d", *tam_n);
         printf("\nDigite 1, 2, 3 ou 4 :\n1- Inserir um elemento na heap\n2- Remover um elemento\n3- Imprimir a Heap\n4- Sair\n");
         scanf("%d", &num);
     }
@@ -53,7 +53,9 @@ int main(){
 void InsereElemento(int valor, int * tam_n, int * vetor){
     vetor = realloc (vetor, ((*tam_n) + 1) * sizeof (int));
     vetor[*tam_n] = valor;
-    *tam_n++;
+    *tam_n = *tam_n + 1 ;
+    printf("\nBBB : %d", *tam_n);
+
 }
 
 void ImprimeElemento(int *tam_n, int * vetor){
